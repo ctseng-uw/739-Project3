6948c942077cd3b7add04c248dcb6cc26cf14616
Sven Hwang <sven.chwen@gmail.com>
2022-03-27 18:34:50 -0400

    heartbeat init

--- gen_proto.sh
+++ gen_proto.sh
@@ -7,5 +7,5 @@ OUT_CPP=includes/${CURR}/
 OUT_GRPC=includes/${CURR}/
 GRPC_PROTOC_PLUGIN=${CURR}/_deps/grpc-build/grpc_cpp_plugin
 
-$PROTOC_BIN --proto_path=${PROTO_DIR} --cpp_out=${OUT_CPP} --grpc_out=${OUT_GRPC} --plugin=protoc-gen-grpc=${GRPC_PROTOC_PLUGIN} hello.proto
+$PROTOC_BIN --proto_path=${PROTO_DIR} --cpp_out=${OUT_CPP} --grpc_out=${OUT_GRPC} --plugin=protoc-gen-grpc=${GRPC_PROTOC_PLUGIN} hello.proto heartbeat.proto
 
--- server.cpp
+++ server.cpp
@@ -1,45 +1,26 @@
-#include "includes/hello.grpc.pb.h"
+#include <iostream>
+#include "heartbeat.h"
 
-#include <grpcpp/ext/proto_server_reflection_plugin.h>
-#include <grpcpp/grpcpp.h>
-#include <signal.h>
+int main(int argc, char** argv) {
+    bool i_am_primary = false;
+    HeartbeatServer heartbeatServer(&i_am_primary);
 
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+    const std::string target_str = "0.0.0.0:50051";
+    grpc::ChannelArguments ch_args;
 
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
+    ch_args.SetMaxReceiveMessageSize(INT_MAX);
+    ch_args.SetMaxSendMessageSize(INT_MAX);
 
-class gRPCServiceImpl final : public hafs::gRPCService::Service {
-  Status hi(ServerContext *context, const hafs::ClientRequest *req,
-            hafs::ServerResponse *reply) {
-    std::string result = "Hello " + req->str();
-    reply->set_str(result);
-    return Status::OK;
-  }
-};
+    HeartbeatClient heartbeatClient(grpc::CreateCustomChannel(
+        target_str, grpc::InsecureChannelCredentials(), ch_args));
 
-int main() {
-  std::string server_address("localhost:50051");
-  gRPCServiceImpl service;
-  grpc::ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.SetMaxSendMessageSize(INT_MAX);
-  builder.SetMaxReceiveMessageSize(INT_MAX);
-  builder.SetMaxMessageSize(INT_MAX);
-
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
-
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+    try
+    {
+        std::cout << heartbeatClient.is_primary() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return 0;
 }
