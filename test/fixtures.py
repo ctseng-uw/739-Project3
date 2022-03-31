89e457489f2bb642bc9812cb82e9dab4855e1d77
Sven Hwang <sven.chwen@gmail.com>
2022-03-31 15:39:26 -0600

    update test

--- test/fixtures.py
+++ test/fixtures.py
@@ -5,43 +5,7 @@ import asyncssh
 import pytest
 import asyncio
 from typing import List
-
-server_addrs = ["node0", "node1"]
-client_addrs = ["node2", "node3"]
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def compile():
-    asyncssh.set_log_level(100)
-
-    async def run_in_build(cmd):
-        proc = await asyncio.create_subprocess_shell(
-            cmd, cwd="../build", stdout=asyncio.subprocess.PIPE
-        )
-        await proc.communicate()
-        assert proc.returncode == 0
-
-    await run_in_build("cmake ..")
-    await run_in_build("make -j")
-    promises = []
-    for s in server_addrs:
-        promises.append(
-            run_in_build(f"scp -o 'StrictHostKeyChecking=no' server {s}:/tmp/server")
-        )
-    for c in client_addrs:
-        promises.append(
-            run_in_build(f"scp -o 'StrictHostKeyChecking=no' client {c}:/tmp/client")
-        )
-        promises.append(
-            run_in_build(f"scp -o 'StrictHostKeyChecking=no' mfsfuse {c}:/tmp/mfsfuse")
-        )
-    await asyncio.wait(promises)
-    logging.info("Compile Done")
-
-
-@pytest.fixture(scope="session")
-def event_loop():
-    return asyncio.get_event_loop()
+from .conftest import server_addrs, client_addrs
 
 
 @pytest.fixture
