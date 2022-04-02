251447a88bc059f165da17380a675873137e413c
PHChuang <pinghanchuang30@gmail.com>
2022-04-02 08:43:37 -0600

    client change server

--- BlockStoreClient.cpp
+++ BlockStoreClient.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "includes/blockstore.grpc.pb.h"
 #include "includes/blockstore.pb.h"
@@ -17,45 +18,81 @@ using grpc::ClientContext;
 
 class BlockStoreClient {
  public:
-  BlockStoreClient(std::string target_str = "10.10.1.1:50051") {
+  BlockStoreClient(int current_server=0, bool designated_server=true)
+    : current_server(current_server), designated_server(designated_server) {
+    server_ip = std::vector<std::string>(
+      {"node0.hadev2.advosuwmadison.emulab.net:50051",
+        "node1.hadev2.advosuwmadison.emulab.net:50051"});
+
     grpc::ChannelArguments ch_args;
 
     ch_args.SetMaxReceiveMessageSize(INT_MAX);
     ch_args.SetMaxSendMessageSize(INT_MAX);
 
     stub_ = hadev::BlockStore::NewStub((grpc::CreateCustomChannel(
-        target_str, grpc::InsecureChannelCredentials(), ch_args)));
+        server_ip[current_server], grpc::InsecureChannelCredentials(), ch_args)));
   }
 
   int Write(const int64_t addr, const std::string &data) {
-    std::string copy(data, 0, 4096);
-    copy.resize(4096);
-    hadev::WriteRequest request;
-    request.set_addr(addr);
-    request.set_data(copy);
-    ClientContext context;
-    hadev::WriteReply reply;
-    auto status = stub_->Write(&context, request, &reply);
-    if (!status.ok()) {
-      return -1;
+    while (true) {
+      std::string copy(data, 0, 4096);
+      copy.resize(4096);
+      hadev::WriteRequest request;
+      request.set_addr(addr);
+      request.set_data(copy);
+      ClientContext context;
+      hadev::WriteReply reply;
+      auto status = stub_->Write(&context, request, &reply);
+      if (!status.ok()) {
+        if (!designated_server) {
+          puts("ChangeServer in Write");
+          ChangeServer();
+          continue;
+        }
+        return -1;
+      }
+      return 0;
     }
-    return 0;
   }
 
   std::string Read(const int64_t addr) {
-    hadev::ReadRequest request;
-    request.set_addr(addr);
-    ClientContext context;
-    hadev::ReadReply reply;
-    auto status = stub_->Read(&context, request, &reply);
-    if (!status.ok()) {
-      return nullptr;
+    while (true) {
+      hadev::ReadRequest request;
+      request.set_addr(addr);
+      ClientContext context;
+      hadev::ReadReply reply;
+      auto status = stub_->Read(&context, request, &reply);
+      if (!status.ok()) {
+        if (!designated_server) {
+          puts("ChangeServer in Read");
+          ChangeServer();
+          continue;
+        }
+        return nullptr;
+      }
+      return reply.data();
     }
-    return reply.data();
   }
 
   ~BlockStoreClient() { stub_.release(); }
 
  private:
   std::unique_ptr<hadev::BlockStore::Stub> stub_;
+  std::vector<std::string> server_ip;
+  int current_server;
+  bool designated_server;
+
+  void ChangeServer() {
+    current_server = 1 - current_server;
+
+    stub_.release();
+
+    grpc::ChannelArguments ch_args;
+
+    ch_args.SetMaxReceiveMessageSize(INT_MAX);
+    ch_args.SetMaxSendMessageSize(INT_MAX);
+
+    stub_ = hadev::BlockStore::NewStub((grpc::CreateCustomChannel(
+    server_ip[current_server], grpc::InsecureChannelCredentials(), ch_args)));
+  }
 };
--- client.cpp
+++ client.cpp
@@ -3,11 +3,45 @@
 #include "BlockStoreClient.cpp"
 
 int main(int argc, char **argv) {
-  BlockStoreClient hadev(argv[1][0] == '0' ? "10.10.1.1:50051"
-                                           : "10.10.1.2:50051");
-  if (argv[2][0] == 'r') {
-    std::cout << hadev.Read(std::atoi(argv[3]));
+  int server_id;
+  int64_t addr;
+  std::string data;
+  bool is_read;
+  bool designated_server;
+
+  if (argc == 3) {
+    server_id = 0;
+    addr = std::atoi(argv[2]);
+    is_read = true;
+    designated_server = false;
+  }
+  else if (argc == 4) {
+    if (argv[2][0] == 'r') {
+      server_id = std::atoi(argv[1]);
+      addr = std::atoi(argv[3]);
+      is_read = true;
+      designated_server = true;
+    }
+    else {
+      server_id = 0;
+      addr = std::atoi(argv[2]);
+      data = std::string(argv[3]);
+      is_read = false;
+      designated_server = false;
+    }
+  }
+  else if (argc == 5) {
+    server_id = std::atoi(argv[1]);
+    addr = std::atoi(argv[3]);
+    data = std::string(argv[4]);
+    is_read = false;
+    designated_server = true;
+  }
+
+  BlockStoreClient hadev(server_id, designated_server);
+  if (is_read) {
+    std::cout << hadev.Read(addr);
   } else {
-    hadev.Write(std::atoi(argv[3]), argv[4]);
+    hadev.Write(addr, data);
   }
 }
