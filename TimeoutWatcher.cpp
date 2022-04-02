c00fef59caefaf4c70228c1341d9c1b6912366d2
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2022-04-01 22:56:19 -0600

    reduce timeout

--- TimeoutWatcher.cpp
+++ TimeoutWatcher.cpp
@@ -8,13 +8,16 @@ class TimeoutWatcher {
   std::condition_variable cv;
   std::mutex cv_m;
   bool heartbeat_received;
+  // First timeout should bit a bit longer
+  std::chrono::seconds timeout = 3s;
 
  public:
   void BlockUntilHeartbeatTimeout() {
     while (true) {
       std::unique_lock<std::mutex> lk(cv_m);
-      auto ret =
-          cv.wait_for(lk, 10s, [this] { return heartbeat_received == true; });
+      auto ret = cv.wait_for(lk, timeout,
+                             [this] { return heartbeat_received == true; });
+      timeout = 1s;  // Reduce subsequent timeout
       if (ret == true) {
         heartbeat_received = false;
       } else {
