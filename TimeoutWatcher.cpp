d819d18e1898fdeb89114b1211605041b34a151a
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2022-04-01 22:26:38 -0600

    remove BlockUntilRcvFirstHeartbeat

--- TimeoutWatcher.cpp
+++ TimeoutWatcher.cpp
@@ -10,23 +10,14 @@ class TimeoutWatcher {
   bool heartbeat_received;
 
  public:
-  void BlockUntilRcvFirstHeartbeat() {
-    std::unique_lock<std::mutex> lk(cv_m);
-    cv.wait(lk);
-  }
-
   void BlockUntilHeartbeatTimeout() {
-    puts("Enter BlockUntilHeartbeatTimeout");
-    int i = 0;  // Debug
     while (true) {
       std::unique_lock<std::mutex> lk(cv_m);
       auto ret =
-          cv.wait_for(lk, 1s, [this] { return heartbeat_received == true; });
+          cv.wait_for(lk, 10s, [this] { return heartbeat_received == true; });
       if (ret == true) {
-        printf("%4d HB Rcvr: Reset timer\n", i++);
         heartbeat_received = false;
       } else {
-        printf("%4d HB Rcvr: Timeout\n", i++);
         return;
       }
     }
@@ -35,8 +26,9 @@ class TimeoutWatcher {
   void NotifyHeartBeat() {
     {
       std::lock_guard<std::mutex> lk(cv_m);
+      puts("Receive heartbeat.");
       heartbeat_received = true;
     }
-    cv.notify_all();
+    cv.notify_one();
   }
 };
